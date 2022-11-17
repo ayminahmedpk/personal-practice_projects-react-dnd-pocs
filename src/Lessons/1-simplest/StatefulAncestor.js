75a4b38ec6d27f841f3cd48c7a86f29fb572d5e2
Aymin Ahmad Alvi VM <aaalvivm@soloinsight.com>
2022-11-17 16:21:26 +0500

    0.1.4

--- src/Lessons/1-simplest/StatefulAncestor.js
+++ src/Lessons/1-simplest/StatefulAncestor.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
+import { useState } from 'react';
+
 import BlockContainer from './BlockContainer';
 import Block from './Block';
 
 const StatefulAncestor = () => {
+
+  const [boxPosition, setBoxPosition] = useState(1)
+
   return (
     <div className="simple-ancestor">
         <BlockContainer>
-            <Block/>
+            {boxPosition == 1 ? <Block/> : ''}
         </BlockContainer>
-        <BlockContainer/>
+        <BlockContainer>
+            {boxPosition == 2 ? <Block/> : ''}
+        </BlockContainer>
     </div>
   )
 }
