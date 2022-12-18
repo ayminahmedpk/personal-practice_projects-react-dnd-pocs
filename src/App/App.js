b254fe0e0a2931c4cb8f2b717984138e984bf49b
ayminahmedpk <ayminahmedpk@gmail.com>
2022-12-18 15:45:34 +0500

    0.1.10

--- src/App/App.js
+++ src/App/App.js
@@ -9,7 +9,8 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 // import Lesson from '../Lessons/1-basic/2-monitoring-drag-and-hover-status/Lesson2';
 // import Lesson from '../Lessons/1-basic/3-hooks-triggering-functions/Lesson3';
 // import Lesson from '../Lessons/1-basic/4-communication/Lesson4';
-import Lesson from '../Lessons/1-basic/5-distinguishing-nested-hover-drop/Lesson5';
+// import Lesson from '../Lessons/1-basic/5-distinguishing-nested-hover-drop/Lesson5';
+import Lesson from '../Lessons/1-basic/6-conditional-drag-drop/Lesson6';
 
 import './style/main.css';
 
---
+++ src/Lessons/1-basic/6-conditional-drag-drop/Lesson6.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import StatefulAncestor from './StatefulAncestor';
+
+const Lesson6 = () => {
+  return (
+    <> <StatefulAncestor/> </>
+  )
+}
+
+export default Lesson6;
